b87d53eecf2735002e6371ea6ef3b90b65898a0f
dt440 <2391899438@qq.com>
2024-10-06 22:09:15 +0800

    C语言课

---
+++ 实验项目2.1.1/实验项目2.1.1/2.1.1.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char c1, c2;
+	c1 = 'a';
+	c2 = 'b';
+	printf("%c,%c\n", c1, c2);
+	printf("%d,%d\n", c1, c2);
+
+	return 0;
+}
